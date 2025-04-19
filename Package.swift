8ee3cdb078429d970093339e9f06e17bc824a002
Rajesh Budhiraja <rajesh.budhiraja@zomato.com>
2025-04-19 21:10:04 +0530

    update version

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", exact: "1.12.0"),
-        .package(url: "https://github.com/BudhirajaRajesh/ZNSTextAttachment", exact: "1.0.0")
+        .package(url: "https://github.com/BudhirajaRajesh/ZNSTextAttachment", exact: "1.0.1")
     ],
     targets: [
         .target(
